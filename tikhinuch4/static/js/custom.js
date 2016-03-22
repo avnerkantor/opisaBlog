a9c2268cdc88104888ac20afdea55b3c87d99038
avnerkantor <avnerkantor@gmail.com>
2016-03-22 09:44:21 +0200

    width

--- tikhinuch4/static/js/custom.js
+++ tikhinuch4/static/js/custom.js
@@ -1,45 +1,14 @@
 //$(function () {
-//    $("#start").show();
-//    $("#achievements").hide();
-//    $("#students").hide();
-//    $("#analyze").hide();
-//    $('#dictionary').hide();
-//    $('#PisaLink').click(function () {
-//        $("#start").hide();
-//        $('#achievements').show();
-//        $("#students").hide();
-//        $('#analyze').hide();
-//        $('#dictionary').hide();
-//    });
-//    $('#Pisa2Link').click(function () {
-//        $("#start").hide();
-//        $('#achievements').hide();
-//        $("#students").show();
-//        $('#analyze').hide();
-//        $('#dictionary').hide();
-//    });
-//    $('#AnalyzeLink').click(function () {
-//        $("#start").hide();
-//        $('#achievements').hide();
-//        $("#students").hide();
-//        $('#dictionary').hide();
-//        $('#analyze').show();
-//    });
-//    $('#SearchLink').click(function () {
-//        $("#start").hide();
-//        $('#achievements').hide();
-//        $("#students").hide();
-//        $('#dictionary').show();
-//        $('#analyze').hide();
-//    });
-//});
-
-
-
-//$(function () {
-//    $('#aa').click(function () {
-//        alert("df");
-//        $('#myiframe').contents().find('#7448-3').click();
-//    });
-//});
-
+//function getParameterByName( name ){
+//    var regexS = "[\\?&]"+name+"=([^&#]*)",
+//  regex = new RegExp( regexS ),
+//  results = regex.exec( window.location.search );
+//  if( results == null ){
+//    return "";
+//  } else{
+//    return decodeURIComponent(results[1].replace(/\+/g, " "));
+//  }
+//}
+//    var result =  getParameterByName('Mode');
+//alert(result)
+//})
