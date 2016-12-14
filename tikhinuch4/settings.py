03ce1ef4ba510a9b91a9eebed9c276974934de27
avnerkantor <avnerkantor@gmail.com>
2016-12-14 18:49:52 +0200

    -

--- tikhinuch4/settings.py
+++ tikhinuch4/settings.py
@@ -13,6 +13,7 @@ SECRET_KEY = '&s^4!!#bz5!=q*i9w3xa!123cn^8@!w^nifb9em228wvb&xwf4'
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
